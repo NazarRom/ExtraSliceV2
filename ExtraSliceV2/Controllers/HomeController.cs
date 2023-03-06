30af8197318103a15efe0bd102392fe5370366ce
NazarRom <nazarrom321@gmail.com>
2023-03-06 21:51:28 +0100

    commit

--- ExtraSliceV2/Controllers/HomeController.cs
+++ ExtraSliceV2/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ExtraSliceV2.Controllers
             return View();
         }
 
+        public IActionResult Restaurante()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
