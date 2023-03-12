78f3703116e5a078fe09c572831c0ea2920b35b6
NazarRom <nazarrom321@gmail.com>
2023-03-12 23:25:11 +0100

    cositas

---
+++ ExtraSliceV2/Controllers/CartaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExtraSliceV2.Controllers
+{
+    public class CartaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Restaurante()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExtraSliceV2/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExtraSliceV2.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public IActionResult Login(string email, string password)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string algo)
+        {
+            return View();
+        }
+
+
+    }
+}
