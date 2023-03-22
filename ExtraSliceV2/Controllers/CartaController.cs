d7fc7643226557612b310f2e09dc445419bc52fb
NazarRom <nazarrom321@gmail.com>
2023-03-22 01:35:20 +0100

    commit

--- ExtraSliceV2/Controllers/CartaController.cs
+++ ExtraSliceV2/Controllers/CartaController.cs
@@ -1,15 +1,19 @@
-﻿using ExtraSliceV2.Models;
+﻿using ExtraSliceV2.Extensions;
+using ExtraSliceV2.Models;
 using ExtraSliceV2.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace ExtraSliceV2.Controllers
 {
     public class CartaController : Controller
     {
         private RepositoryRestaurante repo;
-        public CartaController(RepositoryRestaurante repo)
+        private IMemoryCache memoryCache;
+        public CartaController(RepositoryRestaurante repo, IMemoryCache memoryCache)
         {
             this.repo = repo;
+            this.memoryCache = memoryCache;
         }
 
         public IActionResult Index()
@@ -17,17 +21,101 @@ namespace ExtraSliceV2.Controllers
             List<Restaurante> restaurantes = this.repo.GetRestaurantes();
             return View(restaurantes);
         }
-
-        public IActionResult Restaurante(int idrestaurante)
+        public IActionResult Favoritos()
         {
-            Restaurante restaurante = this.repo.FindRestaurante(idrestaurante);
-            return View(restaurante);
+            List<Producto> productosFavoritos;
+            if (this.memoryCache.Get("FAVORITOS") == null)
+            {
+                productosFavoritos = new List<Producto>();
+            }
+            else
+            {
+                productosFavoritos = this.memoryCache.Get<List<Producto>>("FAVORITOS");
+            }
+            return View(productosFavoritos);
         }
 
-        public IActionResult _ListaProductos(int idrestaurante)
+        public IActionResult Restaurante(int idrestaurante, int? idproducto, int? idfavorito)
         {
-            List<Producto> productos = this.repo.FindProductos(idrestaurante);
-            return PartialView("_ListaProductos", productos);
+            if (idfavorito != null)
+            {
+                List<Producto> productoFavoritos;
+                if (this.memoryCache.Get("FAVORITOS") == null)
+                {
+                    productoFavoritos = new List<Producto>();
+                }
+                else
+                {
+                    productoFavoritos = this.memoryCache.Get<List<Producto>>("FAVORITOS");
+                }
+                //BUSCAMOS PRODUCTO EN BBDD PARA ALMACENARLO EN CACHE
+                Producto producto = this.repo.FindProducto(idfavorito.Value);
+                productoFavoritos.Add(producto);
+                //ALMACENAMOS LOS DATOS EN CACHE
+                this.memoryCache.Set("FAVORITOS", productoFavoritos);
+            }
+
+
+            if (idproducto != null)
+            {
+                List<int> idsProductos;
+                if (HttpContext.Session.GetObject<List<int>>("IdProductos") == null)
+                {
+                    //creamos la lista para los ids
+                    idsProductos = new List<int>();
+                }
+                else
+                {
+                    idsProductos = HttpContext.Session.GetObject<List<int>>("IdProductos");
+                }
+                idsProductos.Add(idproducto.Value);
+                HttpContext.Session.SetObject("IdProductos", idsProductos);
+
+            }
+            //RestauranteProductos restauranteProductos = this.repo.RestProduct(idrestaurante.Value);
+            RestauranteProductos restauranteProductos = new RestauranteProductos
+            {
+                Restaurante = this.repo.FindRestaurante(idrestaurante),
+                Productos = this.repo.FindProductos(idrestaurante)
+            };
+            return View(restauranteProductos);
         }
+       
+
+        public IActionResult CarritoProductos(int? ideliminar)
+        {
+            //tenemos una coleccion de ids y necesitamos
+            //recuperamos los datos de session
+            List<int> idsProductos = HttpContext.Session.GetObject<List<int>>("IdProductos");
+
+            if(idsProductos == null)
+            {
+                ViewData["MENSAJE"] = "No hay productos";
+                return View();
+            }
+            else
+            {
+                if (ideliminar != null)
+                {
+                    //ELIMINAMOS EL ELEMENTO QUE NOS HAN SOLICITADO
+                    idsProductos.Remove(ideliminar.Value);
+                    if (idsProductos.Count == 0)
+                    {
+                        HttpContext.Session.Remove("IdProductos");
+                    }
+                    else
+                    {
+                        //DEBEMOS ACTUALIZAR DE NUEVO SESSION
+                        HttpContext.Session.SetObject("IdProductos", idsProductos);
+                    }
+                }
+
+                List<Producto> productosSession = this.repo.GetProductosSession(idsProductos);
+                return View(productosSession);
+
+            }
+        }
+
+       
     }
 }
--- ExtraSliceV2/Repositories/RepositoryRestaurante.cs
+++ ExtraSliceV2/Repositories/RepositoryRestaurante.cs
@@ -17,11 +17,25 @@ namespace ExtraSliceV2.Repositories
                            select data;
             return consulta.ToList();
         }
-
-        public Restaurante FindRestaurante(int idrestaunrante)
+        public List<Producto> GetProductos()
         {
-            return this.context.Restaurantes.FirstOrDefault(x => x.IdRestaurante == idrestaunrante);
+            var consulta = from data in this.context.Productos
+                           select data;
+            return consulta.ToList();
         }
+
+        public List<Producto> GetProductosSession(List<int> ids)
+        {
+            var consulta = from datos in this.context.Productos
+                           where ids.Contains(datos.IdProducto)
+                           select datos;
+            if (consulta.Count() == 0)
+            {
+                return null;
+            }
+            return consulta.ToList();
+        }
+
 
         public List<Producto> FindProductos(int idrestaurante)
         {
@@ -30,5 +44,32 @@ namespace ExtraSliceV2.Repositories
                            select data;
             return consulta.ToList();
         }
+
+        public Restaurante FindRestaurante(int idrestaunrante)
+        {
+            return this.context.Restaurantes.FirstOrDefault(x => x.IdRestaurante == idrestaunrante);
+        }
+
+        public Producto FindProducto(int idproducto)
+        {
+            return this.context.Productos.FirstOrDefault(x => x.IdProducto == idproducto);
+        }
+
+        //public RestauranteProductos RestProduct (int idrestaurante)
+        //{
+        //    var consultaRest = this.context.Restaurantes.FirstOrDefault(x => x.IdRestaurante == idrestaurante);
+
+        //    var consultaProd = from data in this.context.Productos
+        //                       where data.IdRestaurante == idrestaurante
+        //                       select data;
+
+        //    RestauranteProductos restauranteProductos = new RestauranteProductos
+        //    {
+        //        Restaurante = consultaRest,
+        //        Productos = consultaProd.ToList(),
+
+        //    };
+        //    return restauranteProductos;
+        //}
     }
 }
